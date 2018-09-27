34965f06ae96fd2ad1bff8874b52a23658193794
Am1rah <43459467+Am1rah@users.noreply.github.com>
2018-09-28 03:11:02 +0800

    Week03 here we go.

--- WIE180005/src/Week03/L02Q02.java
+++ WIE180005/src/Week03/L02Q02.java
@@ -1,31 +1,37 @@
 package Week03;
+
 import java.util.Scanner;
 
 public class L02Q02 {
     
-    Scanner calc = new Scanner(System.in);
-    
-    System.out.println("Please input the following numbers.");
-    
-    System.out.println("What is the price of your car?");
-    
-    double p = calc.nextInt();
-    
-    System.out.println("What is the down payment?");
-    
-    double d = calc.nextInt();
-    
-    System.out.println("What is the interest rate (in %)?");
-    
-    double r = calc.nextInt();
-    
-    System.out.println("What is the loan duration within the year?");
-    
-    double y = calc.nextInt();
-    
-    double m = (double) (p - d)*(1 = r*y/100)/(y*12);
-    
-    System.out.println("Your monthly payment should be");
-    
-    System.out.printf("%9.2f", m);
+    public static void main(String[] args) {
+        
+        Scanner calc = new Scanner(System.in);
+            
+        System.out.println("Calcuting your monthly payment.");
+
+        System.out.println("What is the price of your car (in RM)?");
+
+        float p = calc.nextFloat();
+
+        System.out.println("What is the down payment(in RM)?");
+
+        float d = calc.nextFloat();
+
+        System.out.println("What is the interest rate (in %)?");
+
+        float r = calc.nextFloat();
+
+        System.out.println("What is the loan duration within the year?");
+
+        float y = calc.nextFloat();
+
+        float m;
+        
+        m = (float) (p - d)*(1 + r*y/100)/(y*12);
+
+        System.out.println("Your monthly payment should be RM");
+
+        System.out.printf("%20.2f", m); 
+    }
 }
