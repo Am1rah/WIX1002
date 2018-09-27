0bbda2c9c610bfb3723c19ab091fb7487943316d
Am1rah <43459467+Am1rah@users.noreply.github.com>
2018-09-28 03:21:06 +0800

    Week03 here we go.

---
+++ WIE180005 W03/src/Week03/L02Q03.java
@@ -0,0 +1,15 @@
+package Week03;
+import java.util.Random;
+
+public class L02Q03 {
+    public static void main(String[] args) {
+        
+        Random num = new Random();
+        
+        System.out.println("Input 3 integers.");
+        
+        int x, y, z = num.nextInt();
+        
+        int a = 
+    }
+}
